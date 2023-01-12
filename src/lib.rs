ee71348c535e5d002030a8a388b615dbf0451927
Evan McPheron <evaninja2@gmail.com>
2023-01-11 21:39:24 -0600

    Improved main.rs sample

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,41 @@ pub mod char_buffer;
 pub mod line;
 pub mod runner;
 pub mod rendering;
+
+
+pub use rendering::{Vector2, Vector3};
+
+
+pub fn create_cube() -> rendering::Polygon {
+    
+    let mut cube = rendering::Polygon::default();
+    //Top Square
+    cube.insert_vertex(0, v3!(1.0, 1.0, 1.0));
+    cube.insert_vertex(1, v3!(-1.0, 1.0, 1.0));
+    cube.insert_vertex(2, v3!(-1.0, -1.0, 1.0));
+    cube.insert_vertex(3, v3!(1.0, -1.0, 1.0));
+
+    cube.add_edge((0, 1));
+    cube.add_edge((1, 2));
+    cube.add_edge((2, 3));
+    cube.add_edge((3, 0));
+    
+    //Bottom Square
+    cube.insert_vertex(4, v3!(1.0, 1.0, -1.0));
+    cube.insert_vertex(5, v3!(-1.0, 1.0, -1.0));
+    cube.insert_vertex(6, v3!(-1.0, -1.0, -1.0));
+    cube.insert_vertex(7, v3!(1.0, -1.0, -1.0));
+
+    cube.add_edge((4, 5));
+    cube.add_edge((5, 6));
+    cube.add_edge((6, 7));
+    cube.add_edge((7, 4));
+
+    //Connecting the squares
+    cube.add_edge((0, 4));
+    cube.add_edge((1, 5));
+    cube.add_edge((2, 6));
+    cube.add_edge((3, 7));
+
+    cube
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use ascii_renderer::*;
 use char_buffer::CharBuffer;
-use line::Line;
 use runner::{Logic, ProcessReturn, Runner};
 use rendering::{Renderer, Polygon, Camera, Vector3, Vector2};
 use v2;
@@ -19,10 +18,13 @@ impl Logic for MyLogic {
         self.time_offset += delta;
 
         self.renderer.draw(screen_buf);
-        self.renderer.polygons[0].rotation.x += delta * 0.2;
-        self.renderer.polygons[0].rotation.y += delta * 0.4;
+        self.renderer.polygons[0].rotation.x += delta * 0.8;
+        self.renderer.polygons[0].rotation.y += delta * 1.0;
+        self.renderer.polygons[0].rotation.z += delta * 1.2;
 
-        self.renderer.camera.position.z = -10.0 + 2.0 * (self.time_offset * 2.0).sin();
+        self.renderer.polygons[0].scale.x = 1.0 + (self.time_offset * 2.0).sin() * 0.5;
+        self.renderer.polygons[0].scale.y = 1.0 + (self.time_offset * 3.0).sin() * 0.5;
+        self.renderer.polygons[0].scale.z = 1.0 + (self.time_offset * 5.0).sin() * 0.5;
 
         ProcessReturn::Continue
     }
@@ -30,7 +32,7 @@ impl Logic for MyLogic {
 
 fn main() {
 
-    let cube = create_cube();
+    let cube = crate::create_cube();
 
     let mut runner = Runner::new(
         50,
@@ -40,9 +42,9 @@ fn main() {
             renderer: Renderer {
                 polygons: vec![cube],
                 camera: Camera { 
-                    position: v3!(0.0, 0.0, -10.0),
+                    position: v3!(0.0, 0.0, -7.0),
                     rotation: v3!(0.0, 0.0, 0.0),
-                    fov: v2!(0.5, 0.5)
+                    fov: v2!(0.8, 0.8)
                 }
             },
             time_offset: 0.0,
@@ -52,37 +54,5 @@ fn main() {
 }
 
 
-fn create_cube() -> Polygon {
-    
-    let mut cube = Polygon::default();
-    //Top Square
-    cube.insert_vertex(0, v3!(1.0, 1.0, 1.0));
-    cube.insert_vertex(1, v3!(-1.0, 1.0, 1.0));
-    cube.insert_vertex(2, v3!(-1.0, -1.0, 1.0));
-    cube.insert_vertex(3, v3!(1.0, -1.0, 1.0));
 
-    cube.add_edge((0, 1));
-    cube.add_edge((1, 2));
-    cube.add_edge((2, 3));
-    cube.add_edge((3, 0));
-    
-    //Bottom Square
-    cube.insert_vertex(4, v3!(1.0, 1.0, -1.0));
-    cube.insert_vertex(5, v3!(-1.0, 1.0, -1.0));
-    cube.insert_vertex(6, v3!(-1.0, -1.0, -1.0));
-    cube.insert_vertex(7, v3!(1.0, -1.0, -1.0));
-
-    cube.add_edge((4, 5));
-    cube.add_edge((5, 6));
-    cube.add_edge((6, 7));
-    cube.add_edge((7, 4));
-
-    //Connecting the squares
-    cube.add_edge((0, 4));
-    cube.add_edge((1, 5));
-    cube.add_edge((2, 6));
-    cube.add_edge((3, 7));
-
-    cube
-}
 
